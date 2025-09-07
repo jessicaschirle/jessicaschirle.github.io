5bbbf30a058a51c2a023c37b474f4cf7a0fdc918
Jessica Schirle <jessicaschirle@gmail.com>
2025-09-06 20:34:19 -0500

    Updated index.js

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,7 @@ var offset = $('#header').outerHeight();
 
 function getHeader()
 {
-	fetch("blog.html")
+	fetch("./blog.html")
 	.then(response => {
 		if (!response.ok) {
 			throw new Error(`HTTP error, status = ${response.status}`);
@@ -102,3 +102,4 @@ $(document).ready(function(){
       });
   });
   
+
